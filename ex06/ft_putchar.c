e274cfe29bd0e011019984f7264a9352558ab1f2
vadio nyathikazi <vanyathi@c4r8s5.wethinkcode.co.za>
2020-07-13 17:47:16 +0200

    ex06

---
+++ ex06/ft_putchar.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void ft_putnbr(int nb)
+{
+	ft_putnbr(42);
+}
+int main(void)
+{
+	ft_putnbr(42);
+}
---
+++ ex07/ft_print_combn.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void ft_print_combn(int n)
+{
+	if (n > 01)
+	{
+		putchar(n);
+	}
+	else
+		if (n < 10)
+		{
+			putchar(n);
+		}
+}
+int main(void)
+{
+	return 0;
+}
