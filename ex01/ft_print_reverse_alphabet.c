af07b16f13ca901fba1068457d26bd198ae6f718
vadio nyathikazi <vanyathi@c4r8s5.wethinkcode.co.za>
2020-07-13 14:02:11 +0200

    ex01

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void ft_print_alphabet(void)
+{
+	char letter = 'z';
+	while(letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+}
+int main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}
